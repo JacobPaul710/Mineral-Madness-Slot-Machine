398be37073e6f3c0cfc03dbe67fc424242f17234
JacobPaul710 <jacobpaulscott@outlook.com>
2023-03-11 06:50:18 -0800

    Removed commented out code and unused code

--- script.js
+++ script.js
@@ -6,95 +6,27 @@ const stromatolite = "./Project_1_sources/Slot_array_images/stromatolite.png";
 const pictureJasper =
   "./Project_1_sources/Slot_array_images/owyhee_picture_jasper.png";
 const mossAgate = "./Project_1_sources/Slot_array_images/maury_moss_agate.png";
-const carnelian =
-  "./Project_1_sources/Slot_array_images/river_carnelian_agate.png";
-const riverAgate = "./Project_1_sources/Slot_array_images/carnelian.png";
-const oceanAgate = "./Project_1_sources/Slot_array_images/oceanagate.png";
-const limbCast = "./Project_1_sources/Slot_array_images/limbcast.png";
-const fireOpal = "./Project_1_sources/Slot_array_images/fireopal.png";
-const opal = "./Project_1_sources/Slot_array_images/oregon_opal.png";
-const petWood = "./Project_1_sources/Slot_array_images/petwood.png";
 
 const slotArray = [pictureJasper, mossAgate, stromatolite];
 
-// carnelian, riverAgate, oceanAgate, limbCast, fireOpal, opal, petWood]
-
-// const winningCombinationOne = [
-//     [petWood, limbCast, stromatolite],
-//     [petWood, stromatolite, limbCast],
-//     [limbCast, petWood, stromatolite],
-//     [limbCast, stromatolite, petWood],
-//     [stromatolite, petWood, limbCast],
-//     [stromatolite, limbCast, petWood]
-// ];
-
-// const winningCombinationTwo = [
-//     [oceanAgate, riverAgate, carnelian],
-//     [oceanAgate, carnelian, riverAgate],
-//     [riverAgate, oceanAgate, carnelian],
-//     [riverAgate, carnelian, oceanAgate],
-//     [carnelian, oceanAgate, carnelian],
-//     [carnelian, carnelian, oceanAgate],
-
-// ];
-
-// const winningCombinationThree = [
-//     [pictureJasper, mossAgate, stromatolite],
-//     [pictureJasper, stromatolite, mossAgate],
-//     [mossAgate, pictureJasper, stromatolite],
-//     [mossAgate, stromatolite, pictureJasper],
-//     [stromatolite, mossAgate, stromatolite],
-//     [stromatolite, stromatolite, mossAgate],
-
-// ];
-
-// const winningCombinationFour = [
-//     [fireOpal, opal, stromatolite],
-//     [fireOpal, stromatolite, opal],
-//     [opal, fireOpal, stromatolite],
-//     [opal, stromatolite, fireOpal],
-//     [stromatolite, opal, fireOpal],
-//     [stromatolite, fireOpal, opal],
-// ];
 
 //Cached Element References
 
 const reelVariables = document.querySelectorAll(".reel-value");
-// console.log(reelVariables);
-// reelVariables.innerText = slotArray[Math.floor(Math.random() * 6)]
-//an attempt to have the numbers load the 1st time without having to click the button. will revisit.
-
-// const winningIndex = [
-//     [reelVariables[0], reelVariables[1], reelVariables[2]],
-//     [reelVariables[3], reelVariables[4], reelVariables[5]],
-//     [reelVariables[6], reelVariables[7], reelVariables[8]],
-//     [reelVariables[0], reelVariables[3], reelVariables[6]],
-//     [reelVariables[1], reelVariables[4], reelVariables[7]],
-//     [reelVariables[2], reelVariables[5], reelVariables[8]],
-//     [reelVariables[0], reelVariables[4], reelVariables[8]],
-//     [reelVariables[2], reelVariables[4], reelVariables[6]]
-// ]
 
 const machineArm = document.querySelector("#machine-arm");
-// console.log(machineArm);
 
 const paySlot = document.querySelector("#pay-slot");
-// console.log(paySlot);
 
 const roundOutput = document.querySelector(".round-output");
-// console.log(roundOutput);
 
 const remainingBalance = document.querySelector(".remaining-balance");
-// console.log(remainingBalance);
 
 const stakeAmount = document.querySelector("#stake-amount");
-// console.log(stakeAmount);
 
 const stakeInputButton = document.querySelector("#stake-input-button");
-// console.log(stakeInputButton);
 
 const stakeOutput = document.querySelector("#staking-amount-output");
-// console.log(stakeAmount);
 
 const winningReel = document.querySelectorAll(".winning-reel");
 console.log(winningReel);
@@ -111,7 +43,6 @@ paySlot.addEventListener("click", inputMoney);
 machineArm.addEventListener("click", function () {
   if (debounce === true) return;
   debounce = true;
-  // checkForCredits()
   generateArmMotion();
   generateReelGif();
   setTimeout(variableAssigner, 1000);
@@ -123,30 +54,14 @@ stakeInputButton.addEventListener("click", determineStakeAmount);
 
 // Functions
 
-// let credits;
 let winner;
 let balance = 0;
 
-// function checkForCredits () {
-//     if (balance <= 0 || NaN) {
-//         credits = false;
-//         console.log('credits is false')
-//         // alert('You are out of credits. Insert cash into money slot.')
-//     } else
-//     credits = true;
-//     console.log('credits is true')
-// }
-
 function inputMoney() {
   let amountInput = +prompt("Enter desired cash amount");
-  // if (typeof(amountInput) !== 'Number') {
-  //     alert('You must enter a numerical value')
-  // } else {
-  // amountInput = parseInt(amountInput)
   balance += amountInput;
   remainingBalance.innerText = balance;
 }
-// }
 
 function determineStakeAmount() {
   let amountStaked = parseInt(stakeAmount.value);
